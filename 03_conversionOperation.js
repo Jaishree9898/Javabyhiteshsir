b54b6c23d7c02c4ae514a41a46badaa6b2fd6864
Jaishree9898 <jaishree9898@gmail.com>
2023-12-16 13:58:08 +0530

    add

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -1,20 +1,75 @@
-// data-types conversion
+// // // data-types conversion
 
-let score = "30abc";
-console.log(typeof score);   /*   or  console.log(typeof (score));*/
+// // let score = "30abc";
+// // console.log(typeof score);   /*   or  console.log(typeof (score));*/
 
-let valueInNumber=Number(score);  /* because of the datatype Number*/
-console.log(valueInNumber); /* NaN-not a number */
+// // let valueInNumber=Number(score);  /* because of the datatype Number*/
+// // console.log(valueInNumber); /* NaN-not a number */
 
-let isLoggedIn="hi";
-let booleanIsLoggedIn=Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// // let booleanIsLoggedIn=Boolean(isLoggedIn);
+// // console.log(booleanIsLoggedIn);
 
-// score = "30"
-// console.log(typeof score);   
+// // // score = "30"
+// // // console.log(typeof score);   
 
-// let value = Number(score)s
-// console.log(typeof value);
+// // // let value = Number(score)s
+// // // console.log(typeof value);
 
-// value = Number("30abc")s
-// console.log(typeof value);
+// // // value = Number("30abc")s
+// // // console.log(typeof value);
+
+
+// // *************Operations***************//
+
+// let value=3;
+// let negValue= -value;
+// // console.log(negValue);
+
+// // console.log(2+2);
+// // console.log(2-3);
+// // console.log(2*2);
+// // console.log(2**2);
+// // console.log(2/3);
+// // console.log(2%2);
+
+
+let str1="hello";
+let str2="world";
+
+let str3=str1+str2;
+
+console.log(true);
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2!=1);
+// console.log(2==1);
+
+// /*
+// Output
+
+// helloworld
+// 12 
+// 12 
+// 122
+// 32  */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
